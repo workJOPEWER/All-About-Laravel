4e1e8156831f0935d473231119827206450eeadf
workJopewer <arbuztatiana@gmail.com>
2021-03-18 15:38:49 +0200

    bootstrap auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
+	Route::resource('posts', '\App\Http\Controllers\Blog\PostController')->names('blog.posts');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
